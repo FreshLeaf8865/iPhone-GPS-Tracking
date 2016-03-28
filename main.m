1a18a00a8f71b645ead136cc813df530bc1d2ccd
Fresh Leaf <David@Davids-Mac.local>
2016-03-28 17:09:40 +0100

    GPS Tracking

---
+++ main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  iPhoneTracking
+//
+//  Created by Pete Warden on 4/15/11.
+//  Copyright 2011 Apple. All rights reserved.
+//
+
+/***********************************************************************************
+*
+* All code (C) Pete Warden, 2011
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
